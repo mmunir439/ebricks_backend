5574010c3d7108e51faea7ef487ff2a000ea88b7
Muhammad Munir <hrcoder3@gmail.com>
2025-08-28 15:02:09 +0500

    cors is defined

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express=require("express");
+const express = require("express");
 const dotenv = require("dotenv");
-const userRoutes=require("./routes/user.js");
-const connectDB=require("./utils/db.js");
+const cors = require("cors"); // Import cors
+const userRoutes = require("./routes/user.js");
+const connectDB = require("./utils/db.js");
+
 // Load environment variables from .env file
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/user", userRoutes);
+
 // Connect DB
 connectDB();
+
+// CORS Configuration
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // Allow requests from this origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  credentials: true, // Allow cookies if needed
+};
+app.use(cors(corsOptions)); // Use cors middleware
 
 // Routes
 app.get("/", (req, res) => {
@@ -19,5 +31,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(`app is listerning on port: ${port}`);
+  console.log(`app is listening on port: ${port}`);
 });
